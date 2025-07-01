1cda0a1648ab38722aa2eb6e85933b77af0674f9
Vitor Barbosa da Silva <vitor.bsilva33@gmail.com>
2025-07-01 16:17:50 -0300

    Atualiza configurações de dependência e properties

--- src/test/java/br/com/plataforma_rivalix_back/plataforma_rivalix/PlataformaRivalixApplicationTests.java
+++ src/test/java/br/com/plataforma_rivalix_back/plataforma_rivalix/PlataformaRivalixApplicationTests.java
@@ -1,13 +1,18 @@
+// src/test/java/br/com/plataforma_rivalix_back/plataforma_rivalix/PlataformaRivalixApplicationTests.java
 package br.com.plataforma_rivalix_back.plataforma_rivalix;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles; // Importe esta anotação
 
 @SpringBootTest
+@ActiveProfiles("test") // Ativa o perfil 'test' para esta classe de teste
 class PlataformaRivalixApplicationTests {
 
 	@Test
 	void contextLoads() {
+        // Este teste simplesmente verifica se o contexto da aplicação pode ser carregado
+        // com as configurações do perfil 'test'.
 	}
 
 }
