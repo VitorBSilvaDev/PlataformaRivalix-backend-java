6ae8d40f91d39b82e4a9dfdbf59bf02a546b10d5
Vitor Barbosa da Silva <vitor.bsilva33@gmail.com>
2025-07-01 18:51:43 -0300

    altera WebConfig

--- src/main/java/br/com/plataforma_rivalix_back/plataforma_rivalix/config/WebConfig.java
+++ src/main/java/br/com/plataforma_rivalix_back/plataforma_rivalix/config/WebConfig.java
@@ -1,29 +1,18 @@
 package br.com.plataforma_rivalix_back.plataforma_rivalix.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.lang.NonNull;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import org.springframework.beans.factory.annotation.Value;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-
-    @Value("${cors.allowed-origins}") // definindo variável de ambiente que está dentro do applications.properties
-    private String allowedOrigins;
-
-    // CORS(Cross-Origin Resource Sharing) diz ao navegador quais outras origens
-    // possuem permissão para enviar requisições á essa aplicação
     @Override
-    public void addCorsMappings(@NonNull CorsRegistry registry) {
-        String[] origins = java.util.Arrays.stream(allowedOrigins.split(","))
-                .map(String::trim)
-                .toArray(String[]::new);
-
+    public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins(origins)
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                // só esse host pode falar com a sua API
+                .allowedOrigins("https://rivalix-gaming.vercel.app/")
+                .allowedMethods("*")
                 .allowedHeaders("*")
                 .allowCredentials(true);
     }
