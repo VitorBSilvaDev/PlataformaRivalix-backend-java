82d7d06f3438d22212a6ac892d0d3b69fdbb93a5
Vitor Barbosa da Silva <vitor.bsilva33@gmail.com>
2025-07-01 20:56:13 -0300

    Adicionando validação na senha

--- src/main/java/br/com/plataforma_rivalix_back/plataforma_rivalix/model/Usuario.java
+++ src/main/java/br/com/plataforma_rivalix_back/plataforma_rivalix/model/Usuario.java
@@ -42,8 +42,8 @@ public class Usuario {
 	private String email;
 
 	//@Size(min = 8, max = 30, message = "A senha deve ter entre 8 e 30 caracteres")
-	//@Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*()_+=\\-\\[\\]{};':\"|,.<>/?]).*$", message = "A senha deve conter pelo menos uma letra maiúscula, uma minúscula, um número e um caractere especial.")
 	@NotBlank(message = "A senha é obrigatória")
+	@Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*()_+=\\-\\[\\]{};':\"|,.<>/?]).*$", message = "A senha deve conter pelo menos uma letra maiúscula, uma minúscula, um número e um caractere especial.")
 	@Column(name = "senha", columnDefinition = "TEXT", nullable = false)
 	private String senha;
 }
