fd8117714acb7ba7e3fbbbd9925c59db4f1fa9f2
Vitor Barbosa da Silva <vitor.bsilva33@gmail.com>
2025-07-01 18:54:02 -0300

    altera WebConfig

--- src/main/java/br/com/plataforma_rivalix_back/plataforma_rivalix/config/WebConfig.java
+++ src/main/java/br/com/plataforma_rivalix_back/plataforma_rivalix/config/WebConfig.java
@@ -1,19 +1,19 @@
-package br.com.plataforma_rivalix_back.plataforma_rivalix.config;
+    package br.com.plataforma_rivalix_back.plataforma_rivalix.config;
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+    import org.springframework.context.annotation.Configuration;
+    import org.springframework.web.servlet.config.annotation.CorsRegistry;
+    import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
-@Configuration
-public class WebConfig implements WebMvcConfigurer {
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                // só esse host pode falar com a sua API
-                .allowedOrigins("https://rivalix-gaming.vercel.app/")
-                .allowedMethods("*")
-                .allowedHeaders("*")
-                .allowCredentials(true);
+    @Configuration
+    public class WebConfig implements WebMvcConfigurer {
+        @Override
+        public void addCorsMappings(CorsRegistry registry) {
+            registry.addMapping("/**")
+                    // só esse host pode falar com a sua API
+                    .allowedOrigins("https://rivalix-gaming.vercel.app")
+                    .allowedMethods("*")
+                    .allowedHeaders("*")
+                    .allowCredentials(true);
+        }
     }
-}
