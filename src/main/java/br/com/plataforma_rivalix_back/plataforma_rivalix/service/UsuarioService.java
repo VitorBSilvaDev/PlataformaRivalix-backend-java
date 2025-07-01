b37ec5c0ac9d7dadf55b0c0fc4c9a1d7cdfcb9de
Vitor Barbosa da Silva <vitor.bsilva33@gmail.com>
2025-07-01 15:42:04 -0300

    Adiciona @Transactional na função criarUsuario

--- src/main/java/br/com/plataforma_rivalix_back/plataforma_rivalix/service/UsuarioService.java
+++ src/main/java/br/com/plataforma_rivalix_back/plataforma_rivalix/service/UsuarioService.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
+import org.springframework.transaction.annotation.Transactional;
 
 import br.com.plataforma_rivalix_back.plataforma_rivalix.config.SecurityConstants;
 import br.com.plataforma_rivalix_back.plataforma_rivalix.model.Usuario;
@@ -44,6 +45,7 @@ public class UsuarioService {
 				.orElseThrow(() -> new RuntimeException("Usuário não encontrado com o e-mail: " + email));
 	}
 
+	@Transactional
 	public Usuario criarUsuario(Usuario usuario) {
 		if (repository.findByEmail(usuario.getEmail()).isPresent()) {
 			// Lançar ResponseStatusException com a mensagem desejada
